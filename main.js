462dfbe0f4c0b9b9ed7190ca1eec81e94531baad
Heba_Alshiyyab <81535776+HebaShiyyab@users.noreply.github.com>
2021-05-13 02:02:19 +0300

    I am working

--- main.js
+++ main.js
@@ -2,8 +2,20 @@ const express = require("express");
 const app = express();
 const port = 5000;
 const users =["John","Mark"];
+app.use(express.json())
+//Create a middleware function logUsers that logs the users array then invokes the next middleware.
+const logUsers = ((req,res,next)=>{
+    console.log('hello every one');
+    next();
+})
+app.use(logUsers)
+app.use((req,res,next)=>{
+    console.log('hello every one');
+    next();
+})
 app.get("/users", (req, res, next) => {
     res.json(users);
+    next();
 });
 
 app.listen(port, () => {
